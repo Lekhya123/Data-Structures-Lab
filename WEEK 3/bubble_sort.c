e538dd4aca48fa276ed50284139542b815510121
Lekhya123 <69664172+Lekhya123@users.noreply.github.com>
2020-08-19 14:49:47 +0530

    Add files via upload

---
+++ WEEK 3/bubble_sort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void bubble_sort(int a[100] , int n)
+{
+	int t;
+	for(int i=0;i<n - 1;i++){
+		for(int j=0;j<n - i - 1;j++){
+			if(a[j] > a[j+1]){
+				t = a[j];
+				a[j] = a[j+1];
+				a[j+1] = t;
+			}
+		}
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n];
+	for(int i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	bubble_sort(a,n);
+	printf("ELEMENTS AFTER SORTING\n");
+	for(int i=0;i<n;i++)
+	printf("%d ",a[i]);
+}
+	
